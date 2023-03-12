f3d98da8122a89e57772608ce0ef42c0bdef3274
IlyaTom <ilytoma@mail.ru>
2023-03-12 13:28:53 +0300

    Первая задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+string A = Console.ReadLine();
+Console.WriteLine($"Вторая цифра {A[1]}");
