d8af755568dc29e75dc41707537a53f713f75645
IlyaTom <ilytoma@mail.ru>
2023-03-12 13:46:24 +0300

    Третья задача

--- Program.cs
+++ Program.cs
@@ -1,3 +1,37 @@
-﻿Console.Write("Введите трехзначное число: ");
-string A = Console.ReadLine();
-Console.WriteLine($"Вторая цифра {A[1]}");
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int A = int.Parse(Console.ReadLine()!);
+switch (A)
+{
+    case 1:
+    {Console.WriteLine("Рабочий");
+    break;}
+    
+    case 2:
+    {Console.WriteLine("Рабочий");
+    break;}
+    
+    case 3:
+    {Console.WriteLine("Рабочий");
+    break;}
+    
+    case 4:
+    {Console.WriteLine("Рабочий");
+    break;}
+
+    case 5:
+    {Console.WriteLine("Рабочий");
+    break;}
+
+    case 6:
+    {Console.WriteLine("Выходной");
+    break;}
+
+    case 7:
+    {Console.WriteLine("Выходной");
+    break;}
+
+    default:
+    {Console.WriteLine("Нет такого дня!!!!");
+    break;}
+    
+}
