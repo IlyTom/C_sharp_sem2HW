bad3ac815efab5a17557027a682d74fe9317057e
IlyaTom <ilytoma@mail.ru>
2023-03-12 13:39:43 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿Console.Write("Введите трехзначное число: ");
-string A = Console.ReadLine();
-Console.WriteLine($"Вторая цифра {A[1]}");
+﻿Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+if (A/100==0)
+{
+    Console.WriteLine("Третьей цифры нет!!!");
+}
+else
+{
+    string B = Convert.ToString(A);
+    Console.WriteLine($"Третья цифра {B[2]}");
+}
